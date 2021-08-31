132c10a0b81c9f9ec74db5fc471d4b0798eaf28a
Tim Armstrong <git.tim.armstrong@gmail.com>
2021-08-31 21:31:32 +0100

    Update install script.

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env sh
 
+git clone https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
 cargo install exa fd-find ripgrep alacritty --bins
