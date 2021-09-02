2a98edb307b77ca00fdf97f03417c0be1e2c4795
Tim Armstrong <git.tim.armstrong@gmail.com>
2021-09-02 20:44:39 +0100

    Do not lsp insert mode.

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -1,6 +1,9 @@
 
 local lspconfig = require('lspconfig')
 
+vim.lsp.handlers["textDocument/publishDiagnostics"] =      
+     vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics,{ update_in_insert = false })
+
 local on_attach = function(_, bufnr)
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
