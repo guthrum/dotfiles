facb6b33deed7e4ae78f9af8097a3dc4a5088135
Tim Armstrong <git.tim.armstrong@gmail.com>
2021-10-27 20:49:47 +0100

    Sync with local.

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,12 +1,12 @@
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
-
   use 'agude/vim-eldar'
-
   use 'neovim/nvim-lspconfig'
-
   use 'hrsh7th/cmp-nvim-lsp'
+  use 'saadparwaiz1/cmp_luasnip'
+  use 'L3MON4D3/LuaSnip'
+  use 'rust-lang/rust.vim'
 
   use {
     "hrsh7th/nvim-cmp",
